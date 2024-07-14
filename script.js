e12ec945e851e4cbd08a52a5abd01e1e91e8a9c4
David Okpe <okpedavid0@gmail.com>
2024-07-14 18:48:41 +0100

    'semi_finished_v'

--- script.js
+++ script.js
@@ -3,8 +3,45 @@
 const navList = document.querySelectorAll('.list_items')
 const HeroBtn = document.querySelectorAll('.hero_buttons_right button')
 const s =document.querySelector('.category_swiper-button-next')
+const menuToggleBtn=document.querySelector('.menu_open')
+const menu=document.querySelector('.nav_list')
+const listItems=document.querySelectorAll('.list_items')
+
+
+// closeModal
+
+
+// MENU
+
+const menuToggle=()=>{
+   menu.classList.toggle('active')
+}
+
+const closeMenu=(e)=>{
+      menu.classList.remove('active')
+ }
+menuToggleBtn.addEventListener('click',menuToggle)
+
+ listItems.forEach((item)=>{
+ 	item.addEventListener('click',()=>{
+ 		closeMenu()
+ 	})
+ })
+ document.addEventListener('click',(e)=>{
+  if (menu.classList.contains('active') && e.target!== menuToggleBtn) {
+  	closeMenu()
+  }
+  })
+
+
 const List=(param)=>{
 
+//screen width
+let innerWidth =window.innerWidth
+
+window.addEventListener('resize',()=>{
+	innerWidth=window.innerWidth
+});
 param.forEach((item)=>{
 
 	item.addEventListener('click',()=>{
@@ -22,12 +59,10 @@ List(HeroBtn)
 // SWIPER
 
 const swiper1=new Swiper('.swiper1',{
+	loop:true,
 	effect:'cards',
 	grabCursor:true,
-     initialSlide:2,  
-       cardEffect:{
-         
-       },
+   initialSlide:2,  
 	navigation:{
 		nextEl:'.category-swiper-button-next',
 		prevEl:'.category-swiper-button-prev'
@@ -36,16 +71,16 @@ const swiper1=new Swiper('.swiper1',{
 
 })
 const swiper2=new Swiper('.swiper2',{
-	loop:true,
+	loop:'true',
 	effect:'coverflow',
 	grabCursor:true,
-	slidesPerView:5,
-      // coverflowEffect:{
-      // 	rotate:,
-      // 	stretch:,
-      // 	depth:,modifier:,
-      // 	slideShadows:,
-      // }
+	slidesPerView:`${innerWidth > 800 ? 5: 'auto'}`,
+      coverflowEffect:{
+      	rotate:25,
+      	 depth:70,
+      	 modifier:1,
+      	slideShadows:'false',
+      },
 	navigation:{
 		nextEl:'.section4-swiper-button-next',
 		prevEl:'.section4-swiper-button-prev'
@@ -66,3 +101,27 @@ const swiper3 =new Swiper('.swiper3',{
 	}
 })
 
+const swiper4 = new Swiper('.swiper4',{
+	loop:true,
+	freeMode:true,
+	autoplay:{
+	  	delay:1000,
+	  	disableOnInteraction:false,
+	  },
+	  speed:4100,
+   spaceBetween:5,
+	slidesPerView:'auto',
+	grabCursor:true,
+}) 
+const swiper5 = new Swiper('.swiper5',{
+	loop:true,
+	freeMode:true,
+	autoplay:{
+	  	delay:2,
+	  	disableOnInteraction:false,
+	  },
+	  speed:4000,
+   spaceBetween:5,
+	slidesPerView:'auto',
+	grabCursor:true,
+})
